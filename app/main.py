87e0652fd3facc838e8f85a73cdc48087d527dd4
Joao Pedro <joaopedrocostamachado2003@gmail.com>
2025-05-24 18:00:33 -0300

    removing pycache and adding fields to model in request

--- app/main.py
+++ app/main.py
@@ -1,26 +1,38 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 import joblib
 import os
 
 app = FastAPI()
 
 class ModelInput(BaseModel):
-    age: int  # Idade do paciente (numérico)
-    sex: int  # Sexo do paciente (0 = female, 1 = male)
-    bmi: int  # Índice de Massa Corporal (IMC) categorizado
-    children: int  # Número de filhos
-    smoker: int  # Fumante (0 = não fumante, 1 = fumante)
-    region: int  # Região (0 = northeast, 1 = northwest, 2 = southeast, 3 = southwest)
+    age: int  = Field(description="Idade do paciente (numérico)")
+    sex: int  = Field(description="Sexo do paciente (0 = female, 1 = male)")
+    bmi: float  = Field(description="Índice de Massa Corporal (IMC) categorizado")
+    children: int  = Field(description="Número de filhos")
+    smoker: int = Field(description="Fumante (0 = não fumante, 1 = fumante)")
+    region: int = Field(description="Região (0 = northeast, 1 = northwest, 2 = southeast, 3 = southwest)")
 
-MODEL_PATH = os.path.join(os.path.dirname(__file__), "model", "pipeline_rf.joblib")
+MODEL_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "model", "pipeline_rf.joblib")
 model = joblib.load(MODEL_PATH)
 
 @app.post("/predict")
 def predict(input_data: ModelInput):
     try:
-        data = [[input_data.age, input_data.sex, input_data.bmi, input_data.children, input_data.smoker, input_data.region]]
+        data = [
+            [
+                input_data.age,
+                input_data.sex,
+                input_data.bmi,
+                input_data.children,
+                input_data.smoker,
+                input_data.region
+            ]
+        ]
+
         prediction = model.predict(data)
-        return {"prediction": prediction[0]}
+
+        return {"charges": prediction[0]}
+    
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
